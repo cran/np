7a07c4ad7aef6e5911b5a0c504d23c5e72c308e1
Jeffrey S. Racine <racinej@mcmaster.ca>
2010-07-23 00:00:00 +0000

    version 0.40-3

--- R/np.singleindex.R
+++ R/np.singleindex.R
@@ -61,7 +61,7 @@ npindex.formula <-
     tmf <- bws$call[c(1,m)]
     tmf[[1]] <- as.name("model.frame")
     tmf[["formula"]] <- tt
-    umf <- tmf <- eval(tmf)
+    umf <- tmf <- eval(tmf, envir = environment(tt))
 
     tydat <- model.response(tmf)
     txdat <- tmf[, attr(attr(tmf, "terms"),"term.labels"), drop = FALSE]
@@ -318,8 +318,9 @@ npindex.sibandwidth <-
                                     bws=bws$bw)$ksum
 
       if(!no.ex & (no.ey | residuals)){
-        ## want to evaluate on training data for in sample errors even if evaluation x's
-        ## are different from training but no y's are specified
+        ## want to evaluate on training data for in sample errors even
+        ## if evaluation x's are different from training but no y's
+        ## are specified
         
         index.tmean <- npksum(txdat=index,
                               tydat=tydat,
@@ -339,48 +340,70 @@ npindex.sibandwidth <-
 
       index.mean <- model$mean
       
-      ## index.grad is a matrix, one column for each variable, each equal
-      ## to its coefficient beta_i times the first derivative of the
-      ## local-constant model
+      ## index.grad is a matrix, one column for each variable, each
+      ## equal to its coefficient beta_i times the first derivative of
+      ## the local-constant model
       
       index.grad <- as.matrix(model$grad)%*%t(as.vector(bws$beta))
 
       if(!no.ex & (no.ey | residuals)){
-        ## want to evaluate on training data for in sample errors even if evaluation x's
-        ## are different from training but no y's are specified
 
-        index.tmean <- npreg(txdat=index,
-                             tydat=tydat,
-                             bws=bws$bw,
-                             regtype="lc",
-                             gradients=TRUE)$mean
+        ## Want to evaluate on training data for in sample errors even
+        ## if evaluation x's are different from training but no y's
+        ## are specified. Also, needed for variance-covariance matrix
+        ## (uses on ly the training data)
+
+        model <- npreg(txdat=index,
+                       tydat=tydat,
+                       bws=bws$bw,
+                       regtype="lc",
+                       gradients=TRUE)
+
+        index.tmean <- model$mean
+
+        index.tgrad <- as.matrix(model$grad)%*%t(as.vector(bws$beta))        
+
       }
 
 
     }
 
-    if (no.ex) index.tmean <- index.mean
+    if (no.ex) {
+      index.tmean <- index.mean
+    }
 
-    ## June 3 2010, jracine, added vcov methods... thanks to Juan
-    ## Carlos Escanciano <jescanci@indiana.edu> for pushing me on this
-    ## for the Klein and Spady estimator... use index.tmean (training
-    ## X) and index (tx) - need gradients == TRUE in order for this to
-    ## work
+    if (no.ex & gradients) {
+      index.tgrad <- index.grad
+    }
+
+    ## 5/3/2010, jracine, added vcov methods... thanks to Juan Carlos
+    ## Escanciano <jescanci@indiana.edu> for pushing me on this for
+    ## the Klein and Spady estimator... use index.tmean, index.tgrad
+    ## (training X) - need gradients == TRUE in order for this to
+    ## work.
 
     if(bws$method == "ichimura" & gradients == TRUE) {
 
-      ## First row & column of covariance matrix are zero due to
-      ## identification condition that beta_1=0. Note the n^{-1} in
-      ## the E and the \sqrt{n} in the normalization of \hat\beta will
-      ## cancel.
+      ## First row & column of covariance matrix `Bvcov' are zero due
+      ## to identification condition that beta_1=0. Note the n n^{-1}
+      ## n in V^{-1}\Sigma V^{-1} and the \sqrt{n} in the
+      ## normalization of \hat\beta will cancel.
 
       q <- ncol(txdat)
       Bvcov <- matrix(0,q,q)
       dimnames(Bvcov) <- list(bws$xnames,bws$xnames)
 
+      ## Use the weight matrix so we can compute all expectations with
+      ## only one call to npksum (the kernel arguments x\beta do not
+      ## change, only the j for X_{ij} in E(X_{ij}|X_i'\beta)
+
       W <- txdat[,-1,drop=FALSE]
 
-      tyindex <- npksum(txdat = index, tydat = rep(1,length(tydat)), weights = W, bws = bws$bw)$ksum
+      tyindex <- npksum(txdat = index,
+                        tydat = rep(1,length(tydat)),
+                        weights = W,
+                        bws = bws$bw)$ksum
+
       tindex <- npksum(txdat = index, bws = bws$bw)$ksum
 
       ## Need to trap case where k-1=1... ksum will return a 1 D
@@ -397,7 +420,10 @@ npindex.sibandwidth <-
 
       if(is.vector(xmex)) xmex <- matrix(xmex,nrow=1,ncol=length(xmex))
 
-      dg.db.sq <- (W*index.grad[,1])^2
+      ## g^{(1)}=dg/d\beta, first beta normalized to one so this
+      ## simplifies computation (beta's drop out)
+
+      dg.db.sq <- (W*index.tgrad[,1])^2
 
       dg.db.sq.xmex <- sapply(1:length(tydat),function(i){dg.db.sq[i,]*xmex[,i]})      
 
@@ -411,15 +437,20 @@ npindex.sibandwidth <-
     
       dimnames(Bvcov) <- list(bws$xnames,bws$xnames)      
 
+      ## Now export this in an S3 method...
+
     } else if(bws$method == "kleinspady" & gradients == TRUE) {
 
       ## We divide by P(1-P) so test for P=0 or 1...
+      
       keep <- which(index.tmean < 1 & index.tmean > 0)
-      dg.db <- txdat[,-1,drop=FALSE]*index.grad[,1]
+      dg.db <- txdat[,-1,drop=FALSE]*index.tgrad[,1]
+
       ## First row & column of covariance matrix are zero due to
       ## identification condition that beta_1=0. Note the n^{-1} in
       ## the E and the \sqrt{n} in the normalization of \hat\beta will
       ## cancel.
+
       q <- ncol(txdat)
       Bvcov <- matrix(0,q,q)
       Bvcov[-1,-1] <- solve(t(dg.db[keep,])%*%(dg.db[keep,]/(index.tmean[keep]*
@@ -427,7 +458,7 @@ npindex.sibandwidth <-
 
       dimnames(Bvcov) <- list(bws$xnames,bws$xnames)      
 
-      ## Now export these in an S3 method...
+      ## Now export this in an S3 method...
 
     }
 
